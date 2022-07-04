a4f49e84a2c4ae03f3e1f62c709d88f4133a2c84
Francisco Julio <Llanyro@gmail.com>
2022-07-04 17:35:59 +0200

    Update logger.py
    Prob fixed #1

--- logger.py
+++ logger.py
@@ -14,51 +14,71 @@ class Logger:
             os.mkdir(logger_folder)
 
     # region Simple Logs
-    def custom_log(self, logger_file, *args, **kwargs) -> bool:
-        resultado = True
-        try:
-            with open(self.__logger_folder.format(logger_file), "a") as f:
-                for i in args:
-                    f.write(i)
-                    f.write(' ')
-                f.write("\n")
-                f.close()
-        except FileNotFoundError:
-            resultado = False
+    def custom_log(self, logger_file, *args, **kwargs) -> int:
+        """
+            Returns:
+                0: All ok
+                1: args.__len__() == 0 (No args)
+                2: FileNotFoundError
+        """
+        resultado: int = 0
+        if args.__len__() > 0:
+            try:
+                with open(self.__logger_folder.format(logger_file), "a") as f:
+                    f.write(args[0])
+                    for argument in args[1:]:
+                        f.write(' ')
+                        f.write(argument)
+                    f.write("\n")
+                    f.close()
+            except FileNotFoundError:
+                resultado = 2
+        else:
+            resultado = 1
         return resultado
 
-    def log(self, *args, **kwargs) -> bool:
+    def log(self, *args, **kwargs) -> int:
         return self.custom_log(self.__logger_file_log, *args, *kwargs)
 
-    def error(self, *args, **kwargs) -> bool:
+    def error(self, *args, **kwargs) -> int:
         return self.custom_log(self.__logger_file_error, *args, *kwargs)
 
-    def info(self, *args, **kwargs) -> bool:
+    def info(self, *args, **kwargs) -> int:
         return self.custom_log(self.__logger_file_info, *args, *kwargs)
 
     # endregion
     # region Time Logs
-    def custom_log_time(self, logger_file, *args, **kwargs) -> bool:
-        resultado = True
-        try:
-            timestamp = strftime('[%Y-%b-%d %H:%M]')
-            with open(self.__logger_folder.format(logger_file), "a") as f:
-                f.write(f"{timestamp} ")
-                for i in args:
-                    f.write(i)
-                    f.write(' ')
-                f.write("\n")
-                f.close()
-        except FileNotFoundError:
-            resultado = False
+    def custom_log_time(self, logger_file, *args, **kwargs) -> int:
+        """
+            Returns:
+                0: All ok
+                1: args.__len__() == 0 (No args)
+                2: FileNotFoundError
+        """
+        resultado: int = 0
+        if args.__len__() > 0:
+            try:
+                timestamp = strftime('[%Y-%b-%d %H:%M]')
+                with open(self.__logger_folder.format(logger_file), "a") as f:
+                    f.write(f"{timestamp} ")
+                    f.write(args[0])
+                    for argument in args[1:]:
+                        f.write(' ')
+                        f.write(argument)
+                    f.write("\n")
+                    f.close()
+            except FileNotFoundError:
+                resultado = 2
+        else:
+            resultado = 1
         return resultado
 
-    def log_time(self, *args, **kwargs) -> bool:
+    def log_time(self, *args, **kwargs) -> int:
         return self.custom_log_time(self.__logger_file_log, *args, *kwargs)
 
-    def error_time(self, *args, **kwargs) -> bool:
+    def error_time(self, *args, **kwargs) -> int:
         return self.custom_log_time(self.__logger_file_error, *args, *kwargs)
 
-    def info_time(self, *args, **kwargs) -> bool:
+    def info_time(self, *args, **kwargs) -> int:
         return self.custom_log_time(self.__logger_file_info, *args, *kwargs)
     # endregion
